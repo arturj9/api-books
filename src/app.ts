e68b74357cf8a89685058e4f2d66bc9665850262
arturj <arturjardel9@gmail.com>
2024-02-09 19:31:36 -0300

    [add]: Atualizando porta

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
-import 'express-async-errors';
-import express from 'express';
+import "express-async-errors";
+import express from "express";
 
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./lib/swagger_output.json";
 
-import { routes } from './routes';
+import { routes } from "./routes";
 
-import { errorMiddleware } from './middlewares/errorMiddleware';
+import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 8080;
+const hostname = "0.0.0.0";
 
 app.use(express.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
-app.use('/api/v1', routes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+app.use("/api/v1", routes);
 
 app.use(errorMiddleware);
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
